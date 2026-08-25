49659a78a23e554c119cc07899dbfa3e4edb8396
AnaPLysyk <paulalysyk123@gmail.com>
2026-08-25 00:19:04 -0300

    test: validar edicao disponivel no baseline admin

--- steps/intelligence/ui/transacoes/detalhes.steps.ts
+++ steps/intelligence/ui/transacoes/detalhes.steps.ts
@@ -1,3 +1,4 @@
+import { TransactionDetailsPage } from '../../../../pom/intelligence/transaction/details.page';
 import { registrarCaso } from '../../../../utils/common/case-registry';
 import { obterValorObrigatorioDaMassa } from '../../../../utils/data/intelligence';
 import { autenticarAdmin } from '../helpers';
@@ -8,6 +9,7 @@ registrarCaso('INT-100-BASELINE', async (world) => {
   const tguid = obterValorObrigatorioDaMassa('TGUID', process.env.INT_100_TGUID);
   await page.abrirDetalhesDaTransacaoPorTguid(tguid);
   await page.validarDetalhesDaTransacaoCarregados(tguid);
+  await new TransactionDetailsPage(await world.pagina()).validarEdicaoDisponivel();
 });
 
 registrarCaso('INT-100-I1', async (world) => {
